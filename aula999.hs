45f2027042032db111d9a3ee2af7a9983edf0c8a
RosaElvira <lipeboina.boina@hotmail.com>
2019-05-02 12:43:55 -0300

    IO

--- aula999.hs
+++ aula999.hs
@@ -1,1 +1,18 @@
---I/O
+import System.IO
+
+main = do putStrLn "Qual a opcao (0-Sair, 1-Somar, 2-Subtrair, 3-Multiplicar)"
+          opcao <- getLine
+          menu (read opcao :: Int)
+
+menu n
+		| n == 0 = putStrLn "SAIR"
+		| n == 1 = doCount (+)
+		| n == 2 = doCount (-)
+		| n == 3 = doCount (*)
+    | otherwise = putStrLn "INVALIDO"
+
+doCount f = do putStrLn "digite um numero"
+               x <- getLine
+               putStrLn "digite outro número"
+               y <- getLine
+               putStrLn (show (f (read x :: Int) (read y :: Int)))
