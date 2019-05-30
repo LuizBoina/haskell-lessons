af62fcb293be0e1773ff9242e553794e7bb33f08
RosaElvira <lipeboina.boina@hotmail.com>
2019-05-30 12:38:53 -0300

    inserir/remover

--- p2.hs
+++ p2.hs
@@ -79,4 +79,72 @@ toFigure (fig:figs)
             | otherwise = toFigure figs
 
 calcArea [] = "\n"
-calcArea (fig:figs) = (show $ area fig) ++ "\n" ++ (calcArea figs)
+calcArea (fig:figs) = toString fig ++ "\n" ++ (calcArea figs)
+
+toString (Ret ((x1,y1),(x2,y2))) = "Ret " ++ (show ((x1,y1),(x2,y2))) ++ " area = " ++ (show $ area (Ret ((x1,y1),(x2,y2))))
+toString (Tri ((x1,y1),(x2,y2),(x3,y3))) = "Tri " ++ (show ((x1,y1),(x2,y2),(x3,y3))) ++ " area = " ++ (show $ area (Tri ((x1,y1),(x2,y2),(x3,y3))))
+toString (Circ ((x,y), r)) = "Circ " ++ (show ((x,y), r)) ++ " area = " ++ (show $ area (Circ ((x,y), r)))
+
+data Nat = Zero | Succ Nat
+          deriving Show
+
+intNat :: Int -> Nat
+intNat 0 = Zero
+intNat n = Succ (intNat (n-1))
+
+nat2Int :: Nat->Int
+nat2Int Zero = 0
+nat2Int (Succ nat) = 1 + (nat2Int nat)
+
+add :: Nat -> Nat -> Int
+add Zero Zero = 0
+add (Succ m) Zero = 1 + (add m Zero)
+add Zero (Succ n) = 1 + (add Zero n)
+add (Succ m) (Succ n) = 2 + (add m n)
+
+mult :: Nat -> Nat -> Int
+mult Zero _ = 0
+mult (Succ m) n = (nat2Int n) + (mult m n)
+
+data Expr = Val Int | Add Expr Expr | Mul Expr Expr
+            deriving Show
+
+eval (Val h) = h
+eval (Add h o) = eval h + eval o
+eval (Mul h o) = eval h * eval o
+
+{- class Eq a where
+    (==) :: a -> a -> Bool
+    (/=) :: a -> a -> Bool
+    x == y = not (x /= y)
+    x /= y = not (x == y)
+ -}
+
+instance Eq Figure where
+  (Ret (p1,p2)) == (Ret (p3,p4)) =  area (Ret (p1,p2)) == area (Ret (p3,p4))
+  (Circ (p1,f)) == (Circ (p2, r)) = f == r
+  (Tri (p1,p2,p3)) == (Tri (p4,p5,p6)) = area (Tri (p1,p2,p3)) == area (Tri (p4,p5,p6))
+
+instance Eq Expr where
+  (Val o) == (Val b) = o == b
+  (Add e1 e2) == (Add e3 e4) = (eval (Add e1 e2)) == (eval (Add e3 e4))
+  (Mul e1 e2) == (Mul e3 e4) = (eval (Mul e1 e2)) == (eval (Mul e3 e4))
+
+data Arv a = Nil
+          | No (Arv a) a (Arv a)
+          deriving Show
+
+findArv Nil _ = Nada
+findArv (No ra w rb) f
+                  | w == f = Apenas f
+                  | not (vazia ra) = findArv ra f
+                  | otherwise = findArv rb f
+
+vazia Nil = True
+vazia _ = False
+
+findArvBin Nil _ = Nada
+findArvBin (No ra w rb) f
+                  | w == f = Apenas f
+                  | f>w = findArvBin rb f
+                  | otherwise = findArvBin ra f
